d72c2e4c789cae1202217c199c2481c523a1eac8
user06 <45358>
2023-09-28 16:30:55 +0300

    added third activity

--- app/src/main/java/com/example/task/OnBoard.java
+++ app/src/main/java/com/example/task/OnBoard.java
@@ -2,7 +2,9 @@ package com.example.task;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OnBoard extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class OnBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_board);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(this, OnBoardSecond.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slidein, R.anim.slideout);
     }
 }
