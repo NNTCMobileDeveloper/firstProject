6bc7b1555ea6fad5c3c382db24f176c5b9b920f9
user06 <45358>
2023-10-10 16:37:59 +0300

    serhio rames

--- app/src/main/java/com/example/task/OnBoardThree.java
+++ app/src/main/java/com/example/task/OnBoardThree.java
@@ -2,7 +2,9 @@ package com.example.task;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OnBoardThree extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class OnBoardThree extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_board_three);
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slidein, R.anim.slideout);
     }
 }
